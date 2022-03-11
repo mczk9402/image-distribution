3d1aa18f941a90e503123b112aa88e3b008505d5
望月祐汰 <mczk9402@gmail.com>
2022-03-11 22:35:54 +0900

    🐛 APIの変更に合わせて修正

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,13 +8,14 @@ const Home = () => {
 export default Home;
 
 export const getServerSideProps: GetServerSideProps = async () => {
-  const data = await client.get({ endpoint: 'image' });
-  const randomNumber = Math.floor(Math.random() * data.contents.length);
+  const data = await client.get({ endpoint: 'random-image' });
+  const randomNumber = Math.floor(Math.random() * data.images.length);
+  const redirectPath = data.images[randomNumber].image.url;
 
   return {
     redirect: {
       statusCode: 301,
-      destination: data.contents[randomNumber].image.url, // リダイレクト先
+      destination: redirectPath, // リダイレクト先
     },
   };
 };
