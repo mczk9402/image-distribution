f142367194a3b89efb0ec20ac1c7b9fe1bf3bbfe
望月祐汰 <mczk9402@gmail.com>
2022-03-16 23:50:28 +0900

    ✨ 優先公開機能追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,42 @@
-import { GetServerSideProps } from 'next';
-import { client } from 'lib/microcms-client';
+import { GetServerSideProps } from "next";
+import { client } from "lib/microcms-client";
+
+type Images = {
+  fieldId: string;
+  image: {
+    url: string;
+    height: number;
+    width: number;
+  };
+  priority: boolean;
+}[];
+
+type Data = {
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+  revisedAt: string;
+  images: Images;
+};
 
 const Home = () => {
-  return '';
+  return "";
 };
 
 export default Home;
 
 export const getServerSideProps: GetServerSideProps = async () => {
-  const data = await client.get({ endpoint: 'random-image' });
-  const randomNumber = Math.floor(Math.random() * data.images.length);
-  const redirectPath = data.images[randomNumber].image.url;
+  const data: Data = await client.get({ endpoint: "random-image" });
+  const priorityImages = data.images.filter((image) => image.priority);
+  const createRedirectPath = (array: Images) => {
+    const randomNumber = Math.floor(Math.random() * array.length);
+    return array[randomNumber].image.url;
+  };
 
   return {
     redirect: {
       statusCode: 301,
-      destination: redirectPath, // リダイレクト先
+      destination: createRedirectPath(priorityImages.length === 0 ? data.images : priorityImages), // リダイレクト先
     },
   };
 };
